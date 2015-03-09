3278bcd5641f763a6966c183c0ac5f212f1ffa5f
greg <gregorio.jp.mitchell@gmail.com>
2015-03-08 21:33:17 -0700

    mostly working database calls to job postings.  Restructuring to complete pattern for goals,companies,contacts and events.

--- butler.php
+++ butler.php
@@ -1,14 +1,87 @@
 <?php
+/*
+  This file acts as a middle man between the browser (javascript) and gregsList's mysql database
+ */
+
+
 // get requests
 
+// test requests
+/*
 $a = $_POST['a'];
 $b = $_POST['b'];
 $c = $_POST['c'];
 $d = $_POST['d'];
+*/
 
-// process requests
+if (isset($_POST['func']))
+    {
+        // echo getPostings(1);
+        $func = $_POST['func'];
+        if ($func === "getPostings")
+            {
+                echo getPostings(1);
+                //echo "got it";
+            }
+        else
+            {
+                echo "func = " . $_POST['func'];
+            }
+    }
+
+function getPostings($user)
+{
+    $mysqli = connectToDB();
+    $postings = []; // empty container
+    $query = "select url from users inner join user_postings on user_postings.user=users.id ";
+    $query .= "inner join postings on user_postings.posting=postings.id ";
+    $query .= "where users.id = $user";
 
 
-echo "a = $a, b=$b, c=$c, d=$d";
+   if ($sql=$mysqli->prepare($query))
+        {
+            $sql->execute();
+            // bind results
+            $sql->bind_result($i);
+            while($sql->fetch()) // loop over results and push into array
+                array_push($postings,$i);
+            // print_r($categories);
+            return json_encode($postings);
+        }
+}
+
+/*
+  Establish a connection to the gregsList database, and return the connection.
+ */
+function connectToDB(){
+    require './credentials.php';  // username, hostname,password
+    
+    $mysqli = new mysqli($hostname, $username, $password, "gregsList");
+    if ($mysqli->connect_errno)
+        {
+            echo "Failed to connect to MySQL: (" . 
+                $mysqli->connect_errno. ") " . 
+                $mysqli->connect_error;
+        }
+    return $mysqli;
+
+}
+
+/*
+  Interface for sending a query using prepared statements.
+  Returns true if the query is successful, false otherwise
+ */
+function preparedStatement($query){
+    $mysqli = connectToDB();
+    if( $sql = $mysqli->prepare($query)){
+        $sql->execute();
+        mysqli_close($mysqli);
+        return true;
+    }
+    mysqli_close($mysqli);
+    return false;
+    
+}
+
 
 ?>
--- index.js
+++ index.js
@@ -8,8 +8,19 @@ $(document).ready
 	// $("#calendar").resizable();
 	
 	setupCalendar();
+	// getPostings();
 
+	var gregsList = new glo(); // main object 
 	
+	// display postings, companies, contacts, goals
+
+	// get and display job postings	
+	getStuff(gregsList.postings);
+	
+
+
+	// console.log("getter = ");
+	// console.log(gregsList.postings.get);
     }
 );
 
@@ -40,6 +51,150 @@ function setupCalendar()
     );
 }
 
+/*
+  A class to contain mappings to functions
+*/
+function glo()  // gregsList Object 
+{
+    this.postings = 
+	{
+	    get: "getPostings",		// php function to call
+	    display: displayTable,	// javascript function to display
+	    table: $("#tableOfPostings")[0],	// a reference to the div container
+	    add: null			// php function to add posting
+	};
+    this.companies =
+	{
+	    get: "getCompanies",
+	    display: displayCompanies,
+	    add: null
+	}
+    this.contacts =
+	{
+	    get: "getContacts",
+	    display: displayContacts,
+	    add: null
+	}
+    this.schedules =
+	{
+	    get: "getSchedules",
+	    display: displaySchedules,
+	    add: null
+	}
+}
+
+/*
+  A middle man to get things from the butler.
+*/
+function getStuff(object)
+{
+    //console.log(getter);
+    //console.log(callback);
+    var getter = object.get;
+    var callback = object.display;
+
+    $.ajax
+    (
+	{
+	    async: true,
+	    url: "butler.php",
+	    type: "post",
+	    dataType: "text",
+	    data:
+	    {
+		// getter: true
+		func: getter
+	    },
+	    success: function(resp)
+	    {
+		//console.log(resp);
+		callback(object, JSON.parse(resp));
+	    }
+	}
+    )
+}
+
+/*
+  A callback function to render responses from getStuff()
+*/
+function displayTable(object, input)
+{
+    var table = object.table;
+    while (table.firstChild)
+	table.removeChild(table.firstChild);
+
+
+    // insert postings retrieved via ajax
+    for (var i = 0; i < input.length; i++)
+    {
+	var tr = createAppendedChildToParent('tr',table);
+	// var th = createAppendedChildToParent('th',tr);
+	var td = createAppendedChildToParent('td',table);
+	var content = document.createTextNode(input[i]);
+	td.appendChild(content);
+    }
+
+
+}
+
+
+function getPostings()
+{
+    $.ajax
+    (
+	{
+	    url: "butler.php",
+	    type: "post",
+	    dataType: "text",
+	    data:
+	    {
+		getPostings: true
+	    },
+	    success: function(resp)
+	    {
+		displayPostings(JSON.parse(resp));
+	    }	
+	}
+    )
+}
+
+function displayPostings(input)
+{
+    var table = $("#tableOfPostings")[0]; //  get table in DOM
+
+    // remove it's children and rebuild them later
+    while (table.firstChild)
+	table.removeChild(table.firstChild);
+    
+    // var table = createAppendedChildToParent('table',postings);
+
+    // insert postings retrieved via ajax
+    for (var i = 0; i < input.length; i++)
+    {
+	var tr = createAppendedChildToParent('tr',table);
+	// var th = createAppendedChildToParent('th',tr);
+	var td = createAppendedChildToParent('td',table);
+	var content = document.createTextNode(input[i]);
+	td.appendChild(content);
+    }
+    
+}
+
+function displayCompanies()
+{
+}
+
+function displayContacts()
+{
+}
+
+function displaySchedules()
+{
+}
+
+function displayGoals()
+{
+}
 
 function testButler ()
 {
@@ -62,4 +217,14 @@ function testButler ()
 	    }
 	}
     );
+}
+
+
+// create a child DOM element of type childType ct and append to parent p
+// return the created child 
+function createAppendedChildToParent( ct ,parent)
+{
+    var c = document.createElement(ct);
+    parent.appendChild(c);
+    return c;
 }
