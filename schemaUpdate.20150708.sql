a4fdcd41f79fb5e040be8b1e891fe171571b3d5b
greg mobile n900t <gregorio.jp.mitchell@gmail.com>
2015-07-09 09:24:27 -0400

    Found syntax error in schemaUpdate.

--- schemaUpdate.20150708.sql
+++ schemaUpdate.20150708.sql
@@ -5,8 +5,8 @@ create table user_schedule_notes (
 	id int primary key unique auto_increment,
 	`user_schedule_id` int,
 	`notes_user_id` int,
-	foreign key (`user_schedule_id`) references user_schedule.id
+	foreign key (`user_schedule_id`) references user_schedule(id)
 		on delete set null on update cascade,
-	foreign key (`notes_user_id`) references notes_user.id
+	foreign key (`notes_user_id`) references notes_user(id)
 		on delete set null on update cascade
 )engine=innodb;      
