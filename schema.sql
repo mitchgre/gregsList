2b51e156a63a3dfaa4094d2111a35384889f6436
greg gs2 <gregorio.jp.mitchell@gmail.com>
2015-07-27 10:05:39 -0700

    dumped schemaUpdate.20150630.sql into main schema.sql - seems to work.

--- schema.sql
+++ schema.sql
@@ -18,6 +18,18 @@ drop table if exists `pass`;
 drop table if exists `users`;
 drop table if exists industries;
 
+# update 2015-06-30
+drop table if exists notes_goal_user;
+drop table if exists notes_industry_user;
+drop table if exists notes_company_user;
+drop table if exists notes_location_user;
+drop table if exists notes_posting_user;
+drop table if exists notes_contact_user;
+drop table if exists notes_event_user;
+drop table if exists notes_user;
+
+
+
 # table to contain users of gregsList
 create table `users`(
        id int primary key unique auto_increment,
@@ -629,4 +641,87 @@ insert into user_schedule (`user`,`schedule`)
        	      (2,1), (2,2);
        
 	
+
+drop table if exists notes;
+create table `notes` (
+       id int primary key unique auto_increment,
+       title varchar(255),
+       `text` text
+)engine=innodb;
+
+
+create table notes_goal_user (
+       id int primary key unique auto_increment,
+       `goal` int,
+       `note` int,
+       `user` int,
+       foreign key (`user`) references users(id)
+      		  on delete set null on update cascade,
+       foreign key (`goal`) references goals(id)
+      		  on delete set null on update cascade,
+       foreign key (`note`) references `notes`(id)
+       	       on delete set null on update cascade
+)engine=innodb;
+
+
+
+create table notes_industry_user (
+       id int primary key unique auto_increment,
+       `note` int,
+       industry int,
+       user int,
+       foreign key (`note`) references `notes`(id)
+       	       on delete set null on update cascade,
+       foreign key (industry) references industries(id)
+       	       on delete set null on update cascade,
+       foreign key (user) references users(id)
+       	       on delete set null on update cascade
+)engine=innodb;
+
+
+
+
+create table notes_company_user (
+       id int primary key unique auto_increment,
+       `note` int,
+       company int,
+       user int,
+       foreign key (`note`) references `notes`(id)
+       	       on delete set null on update cascade,
+       foreign key (company) references companies(id)
+       	       on delete set null on update cascade,
+       foreign key (user) references users(id)
+       	       on delete set null on update cascade
+)engine=innodb;
+
+
+
+create table notes_posting_user (
+       id int primary key unique auto_increment,
+       `note` int,
+       posting int,
+       user int,
+       foreign key (`note`) references `notes`(id)
+       	       on delete set null on update cascade,
+       foreign key (posting) references postings(id)
+       	       on delete set null on update cascade,
+       foreign key (user) references users(id)
+       	       on delete set null on update cascade
+)engine=innodb;
+
+
+# This table should aggregate all user notes eventually.
+create table notes_user (
+       id int primary key unique auto_increment,
+       `note` int,
+       user int,
+       foreign key (`note`) references `notes`(id)
+       	       on delete set null on update cascade,
+       foreign key (user) references users(id)
+       	       on delete set null on update cascade
+)engine=innodb;
+
+
+
+# Need to add sections for resumes and cover letters here.
 
